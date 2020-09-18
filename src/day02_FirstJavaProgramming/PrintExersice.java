49b3ba1d6720fdef9c29b152c69ea35f31d4cf99
cybertekschool <github@cybertekschool.com>
2020-09-18 16:02:16 -0400

    Day02 Class

---
+++ src/day02_FirstJavaProgramming/PrintExersice.java
@@ -0,0 +1,21 @@
+package day02_FirstJavaProgramming;
+
+public class PrintExersice {
+
+    public static void main(String[] args) {
+
+        System.out.println("Tuncay");
+        System.out.println("Oktay");
+        System.out.println("Dorvan");
+        System.out.println("Alison");
+        System.out.println();
+        System.out.println("Hello");
+        System.out.println("Cybertek");
+        System.out.println("McLean");
+
+
+
+
+    }
+
+}
