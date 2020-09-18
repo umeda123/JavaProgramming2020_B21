0f7e92703053500e84160facff9e8fa6f3bdf762
cybertekschool <github@cybertekschool.com>
2020-09-18 14:59:09 -0400

    Day02 Class

--- src/day02_FirstJavaProgramming/HelloCybertek.java
+++ src/day02_FirstJavaProgramming/HelloCybertek.java
@@ -2,4 +2,16 @@ package day02_FirstJavaProgramming;
 
 public class HelloCybertek {
 
+    public static void main(String[] args){
+
+        System.out.println("Hello Cybertek");
+
+        System.out.println("Hello Batch 21");
+
+        System.out.println("My name is: Muhtar");
+
+
+    }
+
+
 }
