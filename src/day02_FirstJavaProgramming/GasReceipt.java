319720e775e601f68cc3cbb89049d6e5fd25b1d8
cybertekschool <github@cybertekschool.com>
2020-09-18 16:15:37 -0400

    Day02 Class

---
+++ src/day02_FirstJavaProgramming/GasReceipt.java
@@ -0,0 +1,21 @@
+package day02_FirstJavaProgramming;
+
+public class GasReceipt {
+
+    public static void main(String[] args) {
+
+        System.out.println();
+        System.out.println("     MCLEAN STORE");
+        System.out.println();
+        System.out.println("2020-09-18  04:12PM");
+        System.out.println();
+        System.out.println("Gallons:        10.870");
+        System.out.println("Price/gallon:  $ 2.089");
+        System.out.println();
+        System.out.println("Fuel total:  $ 22.71");
+
+
+
+    }
+
+}
