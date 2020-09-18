1ceec7cc1211e519acc4196f45493cebb883c9a8
cybertekschool <github@cybertekschool.com>
2020-09-18 15:19:44 -0400

    Day02 Class

---
+++ src/day02_FirstJavaProgramming/Classmates.java
@@ -0,0 +1,16 @@
+package day02_FirstJavaProgramming;
+
+public class Classmates {
+
+    public static void main(String[] args) {
+
+        System.out.println("Mohammed Alattar");
+        System.out.println("Daria Volkau");
+        System.out.println("Abdullah Irfan");
+        System.out.println("Mustafa Tanis");
+        System.out.println("Abdujilil Ahat");
+
+    }
+
+
+}
