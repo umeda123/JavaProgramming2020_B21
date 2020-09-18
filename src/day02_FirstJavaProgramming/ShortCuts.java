94c0c6b7e45486081083dd291a06ab17c8f50af7
cybertekschool <github@cybertekschool.com>
2020-09-18 15:08:26 -0400

    Day02 Class

---
+++ src/day02_FirstJavaProgramming/ShortCuts.java
@@ -0,0 +1,14 @@
+package day02_FirstJavaProgramming;
+
+public class ShortCuts {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hi Everyone, How are you all today?");
+        System.out.println("Today is Friday!");
+        System.out.println("Hope you enjoyed Java Class");
+
+    }
+
+
+}
