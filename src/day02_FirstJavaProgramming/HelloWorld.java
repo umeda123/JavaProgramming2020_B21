ee965263becc197aae2378c05c58f80bda69747e
cybertekschool <github@cybertekschool.com>
2020-09-21 11:33:58 -0400

    Day02 Class

--- src/day02_FirstJavaProgramming/HelloWorld.java
+++ src/day02_FirstJavaProgramming/HelloWorld.java
@@ -9,6 +9,12 @@ public class HelloWorld {
         System.out.println();
         System.out.println("How Are You?");
 
+                           /*
+                           Date: 09/18/2020
+                           Struggles:
+                                   understanding package, class, main method, print statement
+                                   we only spend four hours
+                            */
 
     }
 
