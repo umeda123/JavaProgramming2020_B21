f1493ecd7b76ac1601ea3139e444453e1eabb92a
cybertekschool <github@cybertekschool.com>
2020-09-21 11:06:42 -0400

    Day03 Class

---
+++ src/day03_Variables/Comments.java
@@ -0,0 +1,23 @@
+package day03_Variables;   // package name
+
+
+public class Comments {   //class: where we will be writing our codes
+
+    public static void main(String[] args) {
+        // This is main method, used for executing the codes
+
+        System.out.println("Hello World");
+            // this is print statement, it prints on the console
+            //    we printed hello world
+
+        /*
+            last section we did our very first java programming hello world
+            it was very fun
+            I enjoyed it
+
+         */
+
+    }
+
+
+}
