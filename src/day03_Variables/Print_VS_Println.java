b34f34032a1659120c1faa931b916ead4d1748f8
cybertekschool <github@cybertekschool.com>
2020-09-21 11:53:50 -0400

    Day02 Class

---
+++ src/day03_Variables/Print_VS_Println.java
@@ -0,0 +1,28 @@
+package day03_Variables;
+
+public class Print_VS_Println {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+
+        System.out.println("==============================================");
+
+        System.out.print("Hola");
+        System.out.print("Hola");
+        System.out.print("Hola");
+        System.out.println("How are you");
+        System.out.println("Are you okay?");
+        System.out.print("Yes I am ");
+        System.out.println("How about you?");
+
+        System.out.println("=================================================");
+
+        System.out.println("Hello Everyone, I am learning Java, Java is a programming language and it's so much fun, easy to learn, i am in love with Java");
+
+
+    }
+
+}
