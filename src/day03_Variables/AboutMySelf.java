40cc78c34faf42c7388fa6d094a5bd2f011c8a6d
cybertekschool <github@cybertekschool.com>
2020-09-21 12:50:48 -0400

    Day03 Class

--- src/day03_Variables/AboutMySelf.java
+++ src/day03_Variables/AboutMySelf.java
@@ -1,4 +1,28 @@
 package day03_Variables;
 
 public class AboutMySelf {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Every\n\tMy name is 'Muhtar', my favorite music is \"Believer\", my favorite show is \"Game Of Throne\"");
+
+        System.out.println("My favorite book is \"Java\"");
+
+    }
+
 }
+
+
+/*
+Task:
+    1. create a class called AboutMySelf
+            write a paragraph to tell us about your self
+                Must include:
+                        favorite music
+                        book
+                        TV series
+
+ */
+
+
+
