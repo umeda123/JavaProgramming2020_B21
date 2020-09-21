1a2b0321e3aeb818a8180be50252ef74decb2c8d
cybertekschool <github@cybertekschool.com>
2020-09-21 10:48:38 -0400

    Day03 Class

---
+++ src/day03_Variables/Rectangle.java
@@ -0,0 +1,23 @@
+package day03_Variables;
+
+public class Rectangle {
+
+    public static void main(String[] args) {
+
+        System.out.println();
+        System.out.println("     * * * * *");
+        System.out.println("     * * * * *");
+        System.out.println("     * * * * *");
+        System.out.println("     * * * * *");
+        System.out.println("     * * * * *");
+        System.out.println("     * * * * *");
+        System.out.println("     * * * * *");
+        System.out.println("     * * * * *");
+        System.out.println("     * * * * *");
+        System.out.println("     * * * * *");
+
+
+    }
+
+
+}
