750e6e0b7457caa9e2a742fa273b391759cbeb09
cybertekschool <github@cybertekschool.com>
2020-09-21 12:41:16 -0400

    Day03 Class

---
+++ src/day03_Variables/EscapeSequences.java
@@ -0,0 +1,49 @@
+package day03_Variables;
+
+
+public class EscapeSequences {
+
+    public static void main(String[] args) {
+    // \n:
+        System.out.println("Hello Everyone");
+        System.out.println("My name is Cybertek, I am from Virginia");
+
+        System.out.println("===============================================");
+
+        System.out.println("Hello Everyone\nMy name is Cybertek, I am from Virginia");
+
+        System.out.println("===============================================");
+
+    // \t:
+        System.out.println("\tToday I learned Java");
+        System.out.println("it was so much fun");
+
+        System.out.println("===============================================");
+
+    //  \\: \
+        System.out.println("/"); // prints single front slash
+        System.out.println("\\"); // prints single backslash
+
+        System.out.println("===============================================");
+
+        System.out.println("My name is 'Muhtar'");
+        System.out.println("My name is \'Muhtar\'");
+
+        System.out.println("===============================================");
+        // \":
+        System.out.println("My favorite show is \"Game Of Thrones\"");
+
+
+
+
+    }
+
+
+}
+
+
+
+/*
+Hello Everyone
+My name is YOURNAME, I am from ...
+ */
