074e0c0d074a0d542f39ecd6c1eb64e7be88e3b8
cybertekschool <github@cybertekschool.com>
2020-09-21 15:32:01 -0400

    Day03 Class

---
+++ src/day03_Variables/CalculateRectangle.java
@@ -0,0 +1,15 @@
+package day03_Variables;
+//1. write a program that can calulate the area & perimeter of any rectangle that has inter number
+
+public class CalculateRectangle {
+
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+
+}
