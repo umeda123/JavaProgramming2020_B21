e5376e66248e954fd16de30e3dc5933d66ea1df0
cybertekschool <github@cybertekschool.com>
2020-09-22 11:05:37 -0400

    Day04 Class

---
+++ src/day04_Variables/SalaryCalculator.java
@@ -0,0 +1,33 @@
+package day04_Variables;
+
+public class SalaryCalculator {
+
+    public static void main(String[] args) {
+        // salary: 150000,  taxRate: 0.28
+
+        int salary = 150000;
+        double taxRate = 0.28;
+
+        double totalTax = salary * taxRate;
+        double salaryAfterTax = salary - totalTax;
+
+        System.out.println("Your total tax will be: ");
+        System.out.println(totalTax);
+
+        System.out.println("Your salary after tax will be: ");
+        System.out.println( salaryAfterTax );
+
+
+    }
+
+}
+
+/*
+write a program that can calculate the salary after tax based on the salary and tax rate
+			Ex:
+				if salary = 100000
+				   taxRate = 0.28
+
+				   totalTax = salary * taxRate;
+				   salary after tax = salary - totalTax
+ */
