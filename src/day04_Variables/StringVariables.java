8dd3b70f7a97c3f576872e7995d1c753a7be69f7
cybertekschool <github@cybertekschool.com>
2020-09-22 12:59:54 -0400

    Day04 Class

---
+++ src/day04_Variables/StringVariables.java
@@ -0,0 +1,21 @@
+package day04_Variables;
+
+public class StringVariables {
+
+    public static void main(String[] args) {
+        // firstName, lastName, yourAddress
+        String firstName = "Cybertek";
+        String lastName = "School";
+
+        String fullName =  firstName+' '+lastName;
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(fullName);
+
+
+
+
+    }
+
+}
