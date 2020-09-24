01fe06b0317484fb1a84aa655f553662025d73f8
cybertekschool <github@cybertekschool.com>
2020-09-24 11:38:11 -0400

    Day05 Class

---
+++ src/day05_ArithmeticOperators/Concatenation_Practice.java
@@ -0,0 +1,32 @@
+package day05_ArithmeticOperators;
+
+public class Concatenation_Practice {
+
+    public static void main(String[] args) {
+
+            int houseNumber = 11822;
+            String streetName = "Federalist Way";
+            String cityName ="Fairfax";
+            String state = "VA";
+            int zipCode = 20302;
+
+            String fullAddress = houseNumber+ " "+ streetName+"\n"  + cityName+" " + state+", " +zipCode;
+
+        System.out.println(fullAddress);
+
+        System.out.println("=============================================");
+
+        String brand = "Toyota";
+        String model = "Camry";
+        int year = 2010;
+        int mileage = 55000;
+        double price = 18000;
+        String color = "Red";
+
+        // 2010 Toyota Camry, 55000 miles, Red, $18000
+
+
+
+    }
+
+}
