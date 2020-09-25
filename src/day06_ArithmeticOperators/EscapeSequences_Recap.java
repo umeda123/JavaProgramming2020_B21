fdc42e95408f536505ab1dad5f3d95d6ef5ed129
cybertekschool <github@cybertekschool.com>
2020-09-25 14:35:25 -0400

    Day06 Class

---
+++ src/day06_ArithmeticOperators/EscapeSequences_Recap.java
@@ -0,0 +1,23 @@
+package day06_ArithmeticOperators;
+
+public class EscapeSequences_Recap {
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
+
+
+/*
+ Escape sequences: for text
+        \n: append new line
+        \t: tab space, paragraph space
+        \\: \
+        \": double quote
+
+    Hello Everyone
+    Hello Batch 21
+    Today I have learned a programming language "Java"
+ */
