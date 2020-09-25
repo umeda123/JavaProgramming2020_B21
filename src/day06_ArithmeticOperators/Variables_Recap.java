a518288c8eb03d5281e320a5b81bcf6ca50ce808
cybertekschool <github@cybertekschool.com>
2020-09-25 15:40:44 -0400

    Day06 Class

--- src/day06_ArithmeticOperators/Variables_Recap.java
+++ src/day06_ArithmeticOperators/Variables_Recap.java
@@ -2,7 +2,22 @@ package day06_ArithmeticOperators;
 
 public class Variables_Recap {
 
+    public static void main(String[] args) {
 
+        System.out.println("My name is Cybertek");
+        System.out.println("My name is Cybertek");
+        System.out.println("My name is Cybertek");
+        System.out.println("My name is Cybertek");
+        System.out.println("My name is Cybertek");
+
+        System.out.println("=======================================");
+        String abc123 = "Cybertek School";
+        String schoolName = "Cybertek School"; // camelCase
+        String schoolName2 = "MIT";
+
+
+
+    }
 
 }
 
