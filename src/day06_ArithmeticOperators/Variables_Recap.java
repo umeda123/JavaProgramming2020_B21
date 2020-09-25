93de479824454e035a4712894a2d05dc11b66251
cybertekschool <github@cybertekschool.com>
2020-09-25 15:00:24 -0400

    Day06 Class

---
+++ src/day06_ArithmeticOperators/Variables_Recap.java
@@ -0,0 +1,23 @@
+package day06_ArithmeticOperators;
+
+public class Variables_Recap {
+
+
+
+}
+
+
+    /*
+    Variables: reuse data
+            DataType  variableName = Data;
+
+            date needs to match with dataType
+
+    important primitives:
+            int: integer numbers (whole numbers)
+            double: deciaml numbers
+            char: single character
+            boolean: Ture or False
+
+            double > float > long > int > short > byte
+     */
