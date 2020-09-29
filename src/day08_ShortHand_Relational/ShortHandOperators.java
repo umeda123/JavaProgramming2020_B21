692b457e2ef026bff5d56fd938f5b95f02cc46d5
cybertekschool <github@cybertekschool.com>
2020-09-29 11:40:49 -0400

    Day08 Class

---
+++ src/day08_ShortHand_Relational/ShortHandOperators.java
@@ -0,0 +1,79 @@
+package day08_ShortHand_Relational;
+
+public class ShortHandOperators {
+
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a);  // 10
+            a = 30;
+        System.out.println(a); // 30
+
+        String schoolName = "Cybertek";
+
+           schoolName = "MIT";
+
+        System.out.println(schoolName);
+
+        int x = 100;
+            x -= 62; // x = x - 62; ==> 38
+
+        System.out.println(x);
+
+            double salary = 100000;   // 0.28
+                    salary  -=  salary * 0.28;
+                    // salary -= 28000;
+                    // salary = salary - 28000
+                    // salary = 100000 - 28000 = 72000
+
+        System.out.println(salary);
+
+        int x2 = 999999;
+            x2 -= 25;
+
+        System.out.println(x2);
+
+        double balance = 2000;
+          balance -= 450; // balance = balance - 450
+
+        System.out.println(balance);
+
+            balance -= 1000;
+
+        System.out.println(balance);
+
+        // depositing
+        balance += 5000;
+
+        System.out.println(balance);
+
+        int y2 = 400;
+        y2 += 10000 * 0.1;
+        // y2 += 1000
+        // y2 = 1400
+
+        System.out.println(y2);
+
+        String name = "Cybertek";
+            name += " School" ;
+           // name = name +" School";
+
+        System.out.println(name);
+
+        double totalTax = 100000;
+                totalTax *= 0.28; // totalTax = 100000 * 0.28
+
+        System.out.println(totalTax);
+
+        int bonus =  5000;
+            bonus *= 2+1;  // bonus = 5000 * 3
+
+        System.out.println(bonus);
+
+
+
+
+
+
+    }
+
+}
