c259f296cea47c99da9aaff584165682f5d277ad
cybertekschool <github@cybertekschool.com>
2020-09-29 12:43:17 -0400

    Day08 Class

--- src/day08_ShortHand_Relational/RelationalOperators.java
+++ src/day08_ShortHand_Relational/RelationalOperators.java
@@ -34,6 +34,29 @@ public class RelationalOperators {
 
         System.out.println("======================================");
         // ==, !=
+        boolean b1 =  10 == 10; // true
+        boolean b2 = 10 != 10; //false
+
+        System.out.println(b1);
+        System.out.println(b2);
+
+       boolean b3 = "Cybertek" == "Virginia"; // false
+        System.out.println(b3);
+
+        boolean b4 = "Virginia" == "virginia"; // false
+        System.out.println(b4);
+
+        boolean b5 = "Muhtar" == "Good Guy";  // false
+        System.out.println(b5);
+
+        boolean b6 = "Muhtar" != "Goog Guy"; // true
+        System.out.println(b6);
+
+        int x = 100;
+        boolean isEven =  x % 2 == 0; //100 is evenly divisible by 2
+            // Even number: if a number can be evenly divisible by 2 with 0 remainder
+
+        boolean isOdd = x % 2 != 0; // opposite of even
 
 
 
