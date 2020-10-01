99379a04d4bc97b1a3e58607115336809162b4c3
cybertekschool <github@cybertekschool.com>
2020-10-01 10:57:07 -0400

    Day09 Class

--- src/day09_IfStatement/EligibleToVote2.java
+++ src/day09_IfStatement/EligibleToVote2.java
@@ -15,7 +15,16 @@ public class EligibleToVote2 {
 
         System.out.println(name+" is eligible to vote: "+isEligible);
 
+        String name2 = "John";
+        String c1 = "Canada";
+        String c2 = "USA";
 
+        boolean eligible =  c1 == "USA"   ||   c2 == "USA";
+        //                 "Canada" == "USA"  ||  "USA" == "USA"
+        //                  false || true
+        // true
+
+        System.out.println(name2+" is eligible to vote: "+eligible);
 
 
     }
