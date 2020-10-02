bd2dd7062d75e7c4911aa39b78a7eb2f51fa1ec5
cybertekschool <github@cybertekschool.com>
2020-10-02 16:14:31 -0400

    Day10 Class

---
+++ src/day10_IfElseStatement/IfStatements.java
@@ -0,0 +1,27 @@
+package day10_IfElseStatement;
+
+public class IfStatements {
+
+    public static void main(String[] args) {
+        int score = 45;
+
+        if( score >= 60 ){
+            System.out.println("Congrats");
+        }
+
+        if(score < 60){
+            System.out.println("Keep trying");
+        }
+
+        System.out.println("==============================");
+
+            if(score >= 60){
+                System.out.println("Congrats");
+            }else{
+                System.out.println("Keep trying");
+            }
+
+
+    }
+
+}
