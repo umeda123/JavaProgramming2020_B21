d2e5ec8bbe0993c07ec0236ca4aed369b3397e19
cybertekschool <github@cybertekschool.com>
2020-10-05 15:36:15 -0400

    Day11 Class

--- src/day11_MultiBranchIf/TernaryPractice.java
+++ src/day11_MultiBranchIf/TernaryPractice.java
@@ -20,6 +20,57 @@ public class TernaryPractice {
 
         System.out.println(result2);
 
+        System.out.println("=======================================");
+
+        int age = 25;
+        String citizen = "USA";
+
+        String eligibleToVote = ( age >= 18 && citizen =="USA" ) ? "Eligible to Vote" : "Not Eligible to Vote" ;
+
+        System.out.println(eligibleToVote);
+
+        System.out.println("==============================================");
+
+        int personAge = 18;
+
+        String buyAlcohol = ( personAge >= 21 ) ? "Eligible to buy alcohol" : "Not Eligible to buy alcohol";
+
+        System.out.println(buyAlcohol);
+
+        System.out.println("=================================================");
+
+        int number = 1000;
+
+        String s1 = "";
+        if(number>0){
+            s1 = "Positive";
+        }else if(number < 0){
+            s1 = "Negative";
+        }else{
+            s1 = "Zero";
+        }
+        System.out.println(s1);
+
+
+        String s2 =  ?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 
