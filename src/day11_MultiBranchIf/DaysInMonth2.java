5a9ad6bb28214ef0a385ea30cf7c263852f9a9f1
cybertekschool <github@cybertekschool.com>
2020-10-05 16:40:50 -0400

    Day11 Class

--- src/day11_MultiBranchIf/DaysInMonth2.java
+++ src/day11_MultiBranchIf/DaysInMonth2.java
@@ -9,20 +9,21 @@ public class DaysInMonth2 {
         boolean has30Days = month == 4 || month == 6 || month == 9 || month ==11;
 
         int days = 0 ;
-
         if(has28Days){
             days = 28;
-
         }else if(has30Days){
             days = 30;
-
         }else{
             days = 31;
-
         }
 
         System.out.println(days+" days");
 
+        System.out.println("=================================================");
+
+        int days2 = (has28Days)? 28 :(has30Days)?  30 : 31;
+
+        System.out.println(days2+" days");
 
 
 
