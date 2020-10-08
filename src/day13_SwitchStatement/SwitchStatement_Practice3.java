e8ea697a19f7252e80838e7817f4e1aef26f9e30
cybertekschool <github@cybertekschool.com>
2020-10-08 12:51:44 -0400

    Day13 Class

--- src/day13_SwitchStatement/SwitchStatement_Practice3.java
+++ src/day13_SwitchStatement/SwitchStatement_Practice3.java
@@ -2,6 +2,39 @@ package day13_SwitchStatement;
 
 public class SwitchStatement_Practice3 {
 
+    public static void main(String[] args) {
+        String browser = "cybertek";
+        String selectedBrowser = "";
+
+        switch (browser){ // chrome
+
+            case "chrome": selectedBrowser = "CHROME BROWSER";
+                break;
+
+            case "firefox": selectedBrowser = "FIREFOX BROWSER";
+                break;
+
+            case "opera": selectedBrowser = "OPERA BROWSER";
+                break;
+
+            case "safari": selectedBrowser = "SAFARI BROWSER";
+                break;
+
+            case "edge": selectedBrowser = "EDGE BROWSER";
+                break;
+
+            case "ie": selectedBrowser = "INTERNET EXPLORE BROWSER";
+                break;
+
+            default: selectedBrowser ="INVALID BROWSER NAME";
+
+        }
+
+        System.out.println(selectedBrowser);
+
+
+    }
+
 
 }
 
@@ -15,4 +48,6 @@ task:
 
     outPut:
         CHROME BROWSER IS SELECTED
+
+        12:35
  */
