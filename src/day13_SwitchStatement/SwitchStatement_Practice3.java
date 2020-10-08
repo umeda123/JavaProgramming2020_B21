734e2eb32a7639a25c1a4d9cf33764c528253482
cybertekschool <github@cybertekschool.com>
2020-10-08 12:20:52 -0400

    Day13 Class

---
+++ src/day13_SwitchStatement/SwitchStatement_Practice3.java
@@ -0,0 +1,18 @@
+package day13_SwitchStatement;
+
+public class SwitchStatement_Practice3 {
+
+
+}
+
+/*
+
+
+task:
+    valid browsers: chrome, firefox, opera, safari, edge, ie
+
+    string str = "chrome"
+
+    outPut:
+        CHROME BROWSER IS SELECTED
+ */
