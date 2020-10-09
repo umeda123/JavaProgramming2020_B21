d1341fca62459d1605c92c757071e9c73eeee6b0
cybertekschool <github@cybertekschool.com>
2020-10-09 15:57:31 -0400

    Day14 Class

--- src/day14_Switch_Recap/SalaryCalculator.java
+++ src/day14_Switch_Recap/SalaryCalculator.java
@@ -3,46 +3,36 @@ package day14_Switch_Recap;
 public class SalaryCalculator {
 
     public static void main(String[] args) {
-        int hourlyRate = -25;
-        int weeklyHours = 35;
+        int hourlyRate = 60;
+        int weeklyHours = 60;
         int numberOfWeeks = 50;
 
         int salary = 0;
 
-        if(hourlyRate > 0){
+        if (hourlyRate > 0) {
 
-            if(weeklyHours >1 && weeklyHours<= 65){
+            if (weeklyHours > 1 && weeklyHours <= 65) {
 
-                if(numberOfWeeks > 1 && numberOfWeeks <= 52){
-
+                if (numberOfWeeks > 1 && numberOfWeeks <= 52) {
                     salary = hourlyRate * weeklyHours * numberOfWeeks;
+                    System.out.println("Your salary is: " + salary);
 
-                }else{
-                    System.out.println("number of weeks cannot be less than 1 or greater than 52");
+                } else {
+                    System.err.println("number of weeks cannot be less than 1 or greater than 52");
                 }
 
-            }else{
-                System.out.println("Weekly Hours cannot be less than 1 or greater than 65");
+            } else {
+                System.err.println("Weekly Hours cannot be less than 1 or greater than 65");
             }
 
-        }else{
-            System.out.println("Hourly Rate cannot be Negative or 0");
+        } else {
+            System.err.println("Hourly Rate cannot be Negative or 0");
         }
-
-
-
-
-        if(salary != 0) {
-            System.out.println("Your salary is: " + salary);
-        }
-
-
 
 
 
 
     }
-
 
 
 }
