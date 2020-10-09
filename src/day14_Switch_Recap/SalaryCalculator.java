6bbd6de34f4509f6aaa02b9c1e09b22e3fb1c21f
cybertekschool <github@cybertekschool.com>
2020-10-09 15:49:19 -0400

    Day14 Class

---
+++ src/day14_Switch_Recap/SalaryCalculator.java
@@ -0,0 +1,64 @@
+package day14_Switch_Recap;
+
+public class SalaryCalculator {
+
+    public static void main(String[] args) {
+        int hourlyRate = -25;
+        int weeklyHours = 35;
+        int numberOfWeeks = 50;
+
+        int salary = 0;
+
+        if(hourlyRate > 0){
+
+            if(weeklyHours >1 && weeklyHours<= 65){
+
+                if(numberOfWeeks > 1 && numberOfWeeks <= 52){
+
+                    salary = hourlyRate * weeklyHours * numberOfWeeks;
+
+                }else{
+                    System.out.println("number of weeks cannot be less than 1 or greater than 52");
+                }
+
+            }else{
+                System.out.println("Weekly Hours cannot be less than 1 or greater than 65");
+            }
+
+        }else{
+            System.out.println("Hourly Rate cannot be Negative or 0");
+        }
+
+
+
+
+        if(salary != 0) {
+            System.out.println("Your salary is: " + salary);
+        }
+
+
+
+
+
+
+    }
+
+
+
+}
+
+
+/*
+1. write a program for the salary calculator
+			given info:
+					hourlyRate  40$
+					weeklyHours  45 hours
+					numberOfWeeks 48 weeks
+
+			pre conditions: hourlyRate cannot be negative
+							weekly hours cannot be negative or more than 65
+							numberOfWeeks cannot be more than 52 weeks or less than 1
+
+
+			salary  =  hourlyRate *  weeklyHours * numberOfWeeks
+ */
